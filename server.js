c9e9dbd8657a01cea5a949bdfbc61c27f9369f18
Victor Asava <vickyavicky23@gmail.com>
2021-07-01 20:16:36 +0300

    Serializing of user object

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
 });
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
