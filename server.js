f9bee38bd62698952931a9b8bc680c9f406244e1
Victor Asava <vickyavicky23@gmail.com>
2021-07-01 19:11:35 +0300

    Setting up passport

--- server.js
+++ server.js
@@ -3,17 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
-
-fccTesting(app); //For FCC testing purposes
 app.set('view engine', 'pug');
+
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.route('/').get((req, res) => {
-  res.render('/home/runner/boilerplate-advancednode-2/views/pug/index', {title: 'Hello', message: 'Please login'});
+  res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
 });
 
 const PORT = process.env.PORT || 3000;
